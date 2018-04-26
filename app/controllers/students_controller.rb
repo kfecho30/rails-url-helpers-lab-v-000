61040567c2a69ca6a48cfd33943d3c3ea5641f24
Kelsey Fecho <kfecho30@gmail.com>
2018-04-26 17:21:45 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
+    @student.active ? @student.active = false : @student.active = true
     redirect_to student_path(@student)
   end
 
